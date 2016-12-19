be3a0ee34945c069c1347140fc50d5b9fb8ec2b0
Chris <christopherdakin1@gmail.com>
2016-12-19 17:19:12 -0600

    heroku local and remote working

--- client/views/app.js
+++ client/views/app.js
@@ -1,9 +1,4 @@
-var app = angular.module('myApp', []);
-
-app.controller('formCtrl', function($scope) {
-    $scope.title = "John";
-});
-$scope.dreams = [];
+var app = angular.module('myApp', [$scope]);
 $scope.submit = function() {
     console.log('test')
 }
