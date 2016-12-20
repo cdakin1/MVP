96f385ff9e69de63531a65aac3fd691ec2c16ec7
Chris <christopherdakin1@gmail.com>
2016-12-20 13:47:50 -0600

    fixed a few errors, now going to refactor in hackreactor format

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
-
+//
 app.controller('submitController', function($scope) {
   $scope.dream = {
     title: '',
@@ -11,10 +11,30 @@ app.controller('submitController', function($scope) {
     console.log($scope.dream);
     console.log($scope.dreams);
   };
-
-
-
 });
 
-
-console.log('testetstset');
+//
+// var mainController = function($scope, $http) {
+//   $scope.dream = {};
+//
+//   $http.get('/api/dreams')
+//       .success(function(data) {
+//         $scope.dreams = data;
+//         console.log(data);
+//       })
+//       .error(function(data) {
+//         console.log('Error: ' + data);
+//       });
+//
+//   $scope.createDream = function() {
+//     $http.post('/api/dreams', $scope.dream)
+//           .success(function(data) {
+//             $scope.dream = {};
+//             $scope.dreams = data;
+//             console.log(data);
+//           })
+//           .error(function(data) {
+//             console.log('Error: ' + data);
+//           });
+//   };
+// };
