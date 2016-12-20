27565a9d2fab06d394fb8804cb8699ef650c8cdb
Chris <christopherdakin1@gmail.com>
2016-12-19 18:58:17 -0600

    cleaned up directory, fixed scripts in index

---
+++ client/app.js
@@ -0,0 +1,10 @@
+var app = angular.module('myApp', ['$scope', function(scope) {;
+    $scope.submit = function() {
+        console.log('test')
+    }
+
+    $scope.formCtrl
+}])
+
+
+console.log('testetstset')
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/client/views/index.html');
-})
+
+app.use(express.static('client'));
 
 app.listen(port, function() {
   console.log("listening on " + port);
