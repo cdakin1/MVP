1bd6f52a9dea41e302c246a0cfdbd0e12bb07eee
Chris <christopherdakin1@gmail.com>
2016-12-20 11:40:24 -0600

    commit before attempting to add database stuff

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000;
+  app = express(),
+  port = process.env.PORT || 3000;
 
 
 app.use(express.static('client'));
 
+///
+
 app.listen(port, function() {
-  console.log("listening on " + port);
+  console.log('listening on ' + port);
 });
 
-console.log('hello from node')
+console.log('hello from node');
